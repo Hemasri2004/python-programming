eafe0d7b86a4e82e5f91df60c0dd6db8bed2a4e4
Hemasri2004 <125556084+Hemasri2004@users.noreply.github.com>
2023-11-01 20:37:26 +0530

    Update p35.py

--- p35.py
+++ p35.py
@@ -1,4 +1,3 @@
-'''
 n=int(input())
 l=[]
 for i in range(n):
@@ -6,28 +5,4 @@ for i in range(n):
     l.append(x)
 print(l[0])
 print(l[-1])
-
-n=int(input())
-l=[]
-for i in range(n):
-    x=int(input())
-    l.append(x)
-print(max(l))
-print(min(l))
-print(max(l)-min(l))
-
-4
-1 2 3 4 
-0 1 2 3
-
-'''
-n=int(input())
-l=[]
-for i in range(n):
-    z=int(input())#1 2 8 9
-    l.append(z)
-x=int(input())#position 1
-y=int(input())#new value 2
-l[x]=y
-print(l)
 
