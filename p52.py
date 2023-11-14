26b8e8f900a94dd4a21ca6ea9fe460b90a6f3f3c
Hemasri2004 <125556084+Hemasri2004@users.noreply.github.com>
2023-11-14 20:38:18 +0530

    Add files via upload

---
+++ p52.py
@@ -0,0 +1,7 @@
+l=[]
+n=int(input())
+for i in range(0,n):
+  x=int(input())
+  l.append(x)
+print(max(l)-min(l))
+
---
+++ p53.py
@@ -0,0 +1,8 @@
+l=[]
+n=int(input())
+for i in range(0,n):
+  x=int(input())
+  l.append(x)
+print(l[0])
+print(l[-1])
+
