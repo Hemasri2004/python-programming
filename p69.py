ffbf054a2f98954e604075938789e1ce4d8a5869
Hemasri2004 <125556084+Hemasri2004@users.noreply.github.com>
2023-11-21 19:45:55 +0530

    Add files via upload

---
+++ p69.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=list(map(int,input().split()))
+e=[]
+o=[]
+for i in l:
+    if i%2==0:
+        e.append(i)
+    else:
+        o.append(i)
+e.extend(o)
+print(e)
+        
+        
---
+++ p70.py
@@ -0,0 +1,9 @@
+l=list(map(int,input().split()))
+o=[]
+for i in l:
+    if i%2!=0:
+        o.append(i)
+        o.append(i)
+    else:
+        o.append(i)
+print(o)
