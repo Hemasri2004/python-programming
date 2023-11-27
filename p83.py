1a7c586e19227c77d0eca81ce0e8bc90b93afcfb
Hemasri2004 <125556084+Hemasri2004@users.noreply.github.com>
2023-11-27 22:57:34 +0530

    Add files via upload

---
+++ p83.py
@@ -0,0 +1,17 @@
+n=int(input())
+A=[]
+for i in range(0,n):
+    l=list(map(int,input().split()))
+    A.append(l)
+pd=0
+sd=0
+for i in range(0,n):
+    for j in range(0,n):
+        if i==j:
+            pd=pd+A[i][j]
+        if i+j==n-1 and i!=j:
+            sd=sd+A[i][j]
+print(pd+sd)
+    
+      
+
