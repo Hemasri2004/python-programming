9b1da94aabd11550d03d0f0731d574055b84e5ac
Hemasri2004 <125556084+Hemasri2004@users.noreply.github.com>
2024-02-15 22:22:09 +0530

    Add files via upload

---
+++ sp11.py
@@ -0,0 +1,9 @@
+n=int(input())
+if n>90:
+    print("First")
+elif n>80:
+    print("Second")
+elif n>70:
+    print("Third")
+else:
+    print("Pass")
---
+++ sp13.py
@@ -0,0 +1,6 @@
+n=int(input())
+m=int(input())
+if n==m:
+    print("Equal")
+else:
+    print("Not Equal")
